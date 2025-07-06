010bb78e0addff6b98d9925a5153771364d48ab7
DharaniMurugan01 <2k21dhara@gmail.com>
2025-07-06 21:25:57 +0530

    Update ParaBank.test.ts

--- PlayWright_Project/tests/ParaBank.test.ts
+++ PlayWright_Project/tests/ParaBank.test.ts
@@ -4,9 +4,9 @@ test("Register feature Test",async({page})=>{
     const regBtn=page.locator("//div[@id='loginPanel']/p[2]/a");
     await regBtn.click();
     const fn=page.locator("//input[@id='customer.firstName']");
-    await fn.fill("Gayathri");
+    await fn.fill("Shiyamala");
     const ln=page.locator("//input[@id='customer.lastName']");
-    await ln.fill("R");
+    await ln.fill("m");
     const add=page.locator("//input[@id='customer.address.street']");
     await add.fill("abc street");
     const city=page.locator("//input[@id='customer.address.city']");
@@ -20,11 +20,11 @@ test("Register feature Test",async({page})=>{
     const ssn=page.locator("//input[@id='customer.ssn']");
     await ssn.fill("1234");
     const uname=page.locator("//input[@name='customer.username']");
-    await uname.fill("testdemo190");
+    await uname.fill("sam@123");
     const pword=page.locator("//input[@name='customer.password']");
-    await pword.fill("pass@123");
+    await pword.fill("sam@123");
     const cpass=page.locator("//input[@name='repeatedPassword']");
-    await cpass.fill("pass@123");
+    await cpass.fill("sam@123");
     const reg=page.locator(".button[value='Register']");
     await reg.click();
     const text=page.locator("//div[@id='rightPanel']/p");
@@ -35,9 +35,9 @@ test("Register feature Test",async({page})=>{
 test("Login feature Test",async({page})=>{
     await page.goto("https://parabank.parasoft.com/parabank/index.htm");
     const uname=page.locator("//input[@name='username']");
-    await uname.fill("testdemo123");
+    await uname.fill("sam@123");
     const pword=page.locator("//input[@name='password']");
-    await pword.fill("pass@123");
+    await pword.fill("sam@123");
     const login=page.locator("(//input[@class='button'])[1]");
     await login.click();
     await expect(page).toHaveURL("https://parabank.parasoft.com/parabank/overview.htm");
